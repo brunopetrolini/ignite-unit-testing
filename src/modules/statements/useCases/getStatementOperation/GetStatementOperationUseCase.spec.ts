78f038fbee4e7d97a6b6827e10618219cdc4a1b1
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-04-05 22:42:55 -0300

    ensure to not get one statement if it user non-exists

--- src/modules/statements/useCases/getStatementOperation/GetStatementOperationUseCase.spec.ts
+++ src/modules/statements/useCases/getStatementOperation/GetStatementOperationUseCase.spec.ts
@@ -4,6 +4,7 @@ import { ICreateUserDTO } from "../../../users/useCases/createUser/ICreateUserDT
 import { InMemoryStatementsRepository } from "../../repositories/in-memory/InMemoryStatementsRepository"
 import { CreateStatementUseCase } from "../createStatement/CreateStatementUseCase"
 import { ICreateStatementDTO } from "../createStatement/ICreateStatementDTO"
+import { GetStatementOperationError } from "./GetStatementOperationError"
 import { GetStatementOperationUseCase } from "./GetStatementOperationUseCase"
 
 describe("Get Statement Operation Use Case", () => {
@@ -25,42 +26,56 @@ describe("Get Statement Operation Use Case", () => {
     password: "test123"
   }
 
-  let statementsRepository: InMemoryStatementsRepository
-  let usersRepository: InMemoryUsersRepository
-  let createUserUseCase: CreateUserUseCase
-  let createStatementUseCase: CreateStatementUseCase
-  let getStatementOperationUseCase: GetStatementOperationUseCase
+  let statementsRepository: InMemoryStatementsRepository;
+  let usersRepository: InMemoryUsersRepository;
+  let createUserUseCase: CreateUserUseCase;
+  let createStatementUseCase: CreateStatementUseCase;
+  let getStatementOperationUseCase: GetStatementOperationUseCase;
 
   beforeEach(() => {
-    statementsRepository = new InMemoryStatementsRepository()
-    usersRepository = new InMemoryUsersRepository()
+    statementsRepository = new InMemoryStatementsRepository();
+    usersRepository = new InMemoryUsersRepository();
     createStatementUseCase = new CreateStatementUseCase(
       usersRepository,
       statementsRepository
-    )
+    );
     createUserUseCase = new CreateUserUseCase(usersRepository)
     getStatementOperationUseCase = new GetStatementOperationUseCase(
       usersRepository,
       statementsRepository
-    )
-  })
+    );
+  });
 
   it("Should be able to get one statement", async () => {
     const user = await createUserUseCase.execute(userData)
     const statement = await createStatementUseCase.execute({
       ...statementData,
       user_id: `${user.id}`
-    })
+    });
 
     const response = await getStatementOperationUseCase.execute({
       user_id: `${user.id}`,
       statement_id: `${statement.id}`
-    })
+    });
 
-    expect(response).toHaveProperty("id")
-    expect(response).toHaveProperty("user_id")
-    expect(response.type).toBe(statementData.type)
-    expect(response.amount).toBe(statementData.amount)
-    expect(response.description).toBe(statementData.description)
-  })
+    expect(response).toHaveProperty("id");
+    expect(response).toHaveProperty("user_id");
+    expect(response.type).toBe(statementData.type);
+    expect(response.amount).toBe(statementData.amount);
+    expect(response.description).toBe(statementData.description);
+  });
+
+  it("Should not be able to get one statement if it user not exists", async () => {
+    const user = await createUserUseCase.execute(userData);
+
+    const statement = await createStatementUseCase.execute({
+      ...statementData,
+      user_id: `${user.id}`
+    });
+
+    await expect( async () => await getStatementOperationUseCase.execute({
+      user_id: `${user.id}-invalid`,
+      statement_id: `${statement.id}`
+    })).rejects.toBeInstanceOf(GetStatementOperationError.UserNotFound);
+  });
 })
