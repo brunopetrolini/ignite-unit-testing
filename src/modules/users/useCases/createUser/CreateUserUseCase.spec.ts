e906f153a3f604d5678ae8d0b82b5a431e53419d
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-04-05 22:54:49 -0300

    ensure to create a user

---
+++ src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
@@ -0,0 +1,29 @@
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository"
+import { CreateUserUseCase } from "./CreateUserUseCase"
+import { ICreateUserDTO } from "./ICreateUserDTO"
+
+describe("Create User Use Case", () => {
+  const userData: ICreateUserDTO = {
+    name: "Admin",
+    email: "admin@finapi.com.br",
+    password: "master"
+  }
+
+  let createUserUseCase: CreateUserUseCase;
+  let usersRepository: InMemoryUsersRepository;
+
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository();
+    createUserUseCase = new CreateUserUseCase(usersRepository);
+  });
+
+  it("Should be able to create a user", async() => {
+    const user = await createUserUseCase.execute(userData);
+
+    expect(user).toHaveProperty("id");
+    expect(user).toHaveProperty("password");
+    expect(user.name).toEqual(userData.name);
+    expect(user.email).toEqual(userData.email);
+    expect(user.password).not.toEqual(userData.password);
+  });
+});
