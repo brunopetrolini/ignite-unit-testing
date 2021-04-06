5d98eac0387b557c9d9ea5dcff35cb70cd729128
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-04-06 19:56:32 -0300

    ensure CreateUserController create a new user

---
+++ src/modules/users/useCases/createUser/CreateUserController.spec.ts
@@ -0,0 +1,31 @@
+import request from "supertest";
+import { Connection, createConnection } from 'typeorm';
+
+import { app } from "../../../../app";
+import { ICreateUserDTO } from "./ICreateUserDTO";
+
+describe("Create user controller", () => {
+  const userData: ICreateUserDTO = {
+    name: "Test User",
+    email: "user@test.com",
+    password: "test123"
+  }
+
+  let db: Connection;
+
+  beforeAll( async () => {
+    db = await createConnection();
+    await db.runMigrations();
+  });
+
+  afterAll( async () => {
+    await db.dropDatabase();
+    await db.close();
+  });
+
+  it("Should be able to create a user", async () => {
+    const response = await request(app).post("/api/v1/users").send(userData);
+
+    expect(response.status).toBe(201);
+  });
+});
