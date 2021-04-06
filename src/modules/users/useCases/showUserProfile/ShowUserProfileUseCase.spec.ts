bcc3e98555681442f0f4a95fdf520dec9194e216
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-04-05 22:58:48 -0300

    ensure to get user information by id

---
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.spec.ts
@@ -0,0 +1,34 @@
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository"
+import { CreateUserUseCase } from "../createUser/CreateUserUseCase"
+import { ICreateUserDTO } from "../createUser/ICreateUserDTO"
+import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase"
+
+describe("Show User Profile Use Case", () => {
+  const userData: ICreateUserDTO = {
+    name: "Admin",
+    email: "admin@finapi.com",
+    password: "master"
+  }
+
+  let usersRepository: InMemoryUsersRepository;
+  let createUserUseCase: CreateUserUseCase;
+  let showUserProfileUseCase: ShowUserProfileUseCase;
+
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository();
+    createUserUseCase = new CreateUserUseCase(usersRepository);
+    showUserProfileUseCase = new ShowUserProfileUseCase(usersRepository);
+  });
+
+  it("Should be able to get user information by id", async () => {
+    const user = await createUserUseCase.execute(userData);
+
+    const response = await showUserProfileUseCase.execute(user.id ?? "");
+
+    expect(response).toHaveProperty("id");
+    expect(response).toHaveProperty("password");
+    expect(response.name).toEqual(userData.name);
+    expect(response.email).toEqual(userData.email);
+    expect(response.password).not.toEqual(userData.password);
+  });
+});
