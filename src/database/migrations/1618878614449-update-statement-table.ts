09f46476c08b0ea68419fe0574e47ebfafa95a7e
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-04-19 21:33:37 -0300

    add transfers migration

---
+++ src/database/migrations/1618878614449-update-statement-table.ts
@@ -0,0 +1,18 @@
+import {MigrationInterface, QueryRunner, TableColumn} from "typeorm";
+
+export class updateStatementTable1618878614449 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.addColumn("statements", new TableColumn({
+          name: "transfer_id",
+          type: "uuid",
+          isNullable: true
+        })
+      );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropColumn("statements", "transfer_id");
+    }
+
+}
